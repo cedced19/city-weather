e98a35a74b6a739687b40c454c71d7b8def2f366
Cédric <cedced19@gmail.com>
2015-07-24 22:08:53 +0200

    Inititial commit

---
+++ city-weather/platforms/android/assets/www/js/app.js
@@ -0,0 +1,25 @@
+﻿angular.module('CityWeather', []).controller('CityWeatherCtrl', function ($scope, $http) {
+        
+        $scope.panel = 0;
+
+        $scope.search = function () {
+            var url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.city + "&mode=json&units=metric&cnt=10";
+            $scope.loader = true;
+            $http.get(url).success(function (response) {
+                $scope.panel = 1;
+                $scope.loader = false;
+                $scope.weather = response;
+            }).error(function () {
+                $scope.loader = false;
+                $scope.error = true;
+            });
+
+        }
+
+        $scope.expand = function (e) {
+            var $elem = $(e.currentTarget);
+            $elem.addClass("row_active").siblings().removeClass('row_active');
+        }
+
+        $scope.Math = Math;
+});
